cff3b3bd3ad10aa4b1ca23b85e183aca1c582ddf
caleb765landis <caleb765landis@gmail.com>
2023-07-07 19:09:52 -0600

    added navbar and toast

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import toast from 'react-hot-toast';
 
 import Loader from '../components/Loader';
 
@@ -8,6 +9,9 @@ export default function Home() {
   return (
     <div>
       <Loader show />
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast Me
+      </button>
     </div>
   )
 }
